1585329ff823b26d5802bf953db42b96b37722b8
qazxswedc <sandeep.carmel@gmail.com>
2017-02-15 10:25:15 +0000

    All done, set for testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// var mongoose = require("mongoose");
-// mongoose.connect(process.env.MLAB_URI);
-var request = require("request")
+var MongoClient = require('mongodb').MongoClient;
+var db_url =  process.env.MLAB_URI;
+var request = require("request");
 var express = require("express");
 var app = express();
 
@@ -14,6 +14,8 @@ app.get("/api", function(req, res){
 
 //URL mapping for /api/imagesearch/:query?offset=10
 app.get("/api/imagesearch/:query", function(req, res){
+    
+    //Constant part of API.
 	var api = "https://www.googleapis.com/customsearch/v1?imgSize=medium&searchType=image&key=AIzaSyCROKVCgZesSu6wGg5lrsrGGvnxtJfbSKE&cx=002431131552367704307:fgyfwrbhygs&q=";
 	//Read the term to be searched.
 	var q = req.params.query;
@@ -32,6 +34,8 @@ app.get("/api/imagesearch/:query", function(req, res){
 	var url = api+q+"&start="+ offset;
 	//console.log(url);
 	
+	saveToDb(q);
+	
 	//Get the response JSON
     request({
         url: url,
@@ -49,3 +53,31 @@ app.get("/api/imagesearch/:query", function(req, res){
 app.listen(app.get('port'), function(){
     console.log('listening on :'+ app.get('port'))
 });
+
+
+function saveToDb(query){
+    MongoClient.connect(db_url, function(err, db){
+       if(err){
+           console.log(err);
+           return;
+       }
+        //console.log("Connected successfully to server");
+        
+        //Select the collection
+        var recentSearchs = db.collection('recent_searches');
+        
+        //Query the collection for the tuple to be replaced.
+        recentSearchs.findAndModify(
+            {}, // query
+            [['when','asc']],  // sort order
+            {$set: { 'term' : query, 'when' : new Date()}},//Replacement
+            {},//Options
+            function(err, object ){//Callback
+                if(err)
+                    console.log(err)
+                //console.log(object);
+            });
+
+        db.close();
+    });
+}
